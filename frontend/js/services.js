bec1dfa6e46472d32e7bed8983ffeabf0a9ab41f
AprajitaMishra-CS <mishraaprajita700@gmail.com>
2026-07-17 22:53:53 +0530

    Created Services Page UI and small modifications in existing pages

---
+++ frontend/js/services.js
@@ -0,0 +1,45 @@
+// sidebar
+const menu = document.querySelector(".menuIcon");
+const nav = document.querySelector(".navRight");
+const overlay = document.querySelector(".overlay");
+
+menu.addEventListener("click", ()=>{
+    menu.classList.toggle("active");
+    nav.classList.toggle("active");
+    overlay.classList.toggle("show");
+});
+
+overlay.addEventListener("click", ()=>{
+    menu.classList.remove("active");
+    nav.classList.remove("active");
+    overlay.classList.remove("show");
+});
+
+let nvRight = document.querySelectorAll(".navRight a");
+
+nvRight.forEach((link)=>{
+    link.addEventListener("click",()=>{
+        menu.classList.remove("active");
+        nav.classList.remove("active");
+        overlay.classList.remove("show");
+    });
+});
+
+// scroll to top button
+const topBtn = document.querySelector("#topBtn");
+
+window.onscroll = function(){
+    if(document.body.scrollTop>300 || document.documentElement.scrollTop>300){
+        topBtn.style.display = "block";
+    }
+    else {
+        topBtn.style.display = "none";
+    }
+}
+
+topBtn.onclick = function(){
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+}
